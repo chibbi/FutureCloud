12bd0341e6de3401a748f4704436cfecf245fbcf
chibbi <chehab.djibril@gmail.com>
2020-12-12 22:22:23 +0100

    Organization

---
+++ cloud.js
@@ -0,0 +1,98 @@
+module.exports = function () {
+    const fs = require("fs");
+
+    let sessions = [];
+
+    module.multer = function (mult) {
+        return {
+            storage: mult.diskStorage({
+                destination: function (req, file, cb) {
+                    cb(null, __dirname + "/public/")
+                },
+                filename: function (req, file, cb) {
+                    var [loggedin, user] = isLoggedIn(req.cookies.session);
+                    if (loggedin) {
+                        var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
+                        var fileArt = file.originalname.split(".");
+                        fileArt = fileArt[fileArt.length - 1];
+                        var fileCustomName = createFileName() + "." + fileArt;
+                        var path = __dirname + "/public/" + fileCustomName;
+                        if (req.body.fileName != undefined && req.body.fileName != "") {
+                            var newFile = { "name": req.body.fileName, "path": "/" + fileCustomName, "owner": user, "share": "admin" };
+                        } else {
+                            var newFile = { "name": file.originalname, "path": "/" + fileCustomName, "owner": user, "share": "admin" };
+                        }
+                        usersJson[Object.keys(usersJson).length] = newFile;
+                        fs.writeFileSync(__dirname + "/userDB/" + user + ".json", Buffer.from(JSON.stringify(usersJson), "utf8"));
+                        return cb(null, fileCustomName);
+                    } else {
+                        return cb(new Error('Not Logged in'));
+                    }
+                }
+            })
+        }
+    }
+
+    module.delFile = function (file, user) {
+        var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
+        console.log(usersJson);
+        for (const [key, value] of Object.entries(usersJson)) {
+            if (value.path == file) {
+                delete usersJson[key];
+            }
+        }
+        fs.writeFileSync(__dirname + "/userDB/" + user + ".json", Buffer.from(JSON.stringify(usersJson), "utf8"));
+    }
+
+    module.createSession = function (user) {
+        var exists = true;
+        var num;
+        while (exists) {
+            num = Math.floor(Math.random() * 1E18);
+            for (var i = 0; i < sessions.length; i++) {
+                if (num == sessions[i].num) {
+                    exists = true;
+                }
+            }
+            sessions.push({ num, user });
+            break;
+        }
+        return num;
+    }
+
+    module.isLoggedIn = function (session) {
+        return isLoggedIn(session);
+    }
+
+    function isLoggedIn(session) {
+        for (var i = 0; i < sessions.length; i++) {
+            if (session == sessions[i].num) {
+                user = sessions[i].user;
+                return [true, sessions[i].user];
+            }
+        }
+        return [false, ""];
+    }
+
+    function createFileName() {
+        var exists = true;
+        var num;
+        var dir = fs.readdirSync(__dirname + "/public");
+        var allexisting = [];
+        for (var i = 0; i < dir.length; i++) {
+            var name = dir[i].split(".");
+            name = name[0];
+            allexisting.push(name);
+        }
+        while (exists) {
+            num = Math.floor(Math.random() * 1000000000000000000);
+            for (var i = 0; i < allexisting.length; i++) {
+                if (num == allexisting[i].num) {
+                    exists = true;
+                } else { exists = false }
+            }
+        }
+        return num;
+    }
+    return module;
+};
--- router.js
+++ router.js
@@ -4,74 +4,30 @@ module.exports = function (app) {
     const fs = require("fs");
     const multer = require('multer');
     const log = require("./logging")();
+    const cloud = require("./cloud")();
 
-    let sessions = [];
-    /*
-    TODO: TRY THAT IF still not working
-    const multerConfig = {
-      storage : multer.diskStorage({
-        destination: function (req, file, next) {
-          next(null, __dirname + "/public/")
-        },
-        filename: function (req, file, next) {
-      
-          let imageId = encryption.generateId();
-          let imageNameWithId = imageId.substr(0,10);
-          imagesNames.push({name:imageNameWithId,id:imageId});
-          let fileName = "" + imageNameWithId + "_" + file.originalname;
-      
-          next(null, fileName)
-        }
-      })
-    };
-    
-    */
-    const multerConfig = {
-        storage: multer.diskStorage({
-            destination: function (req, file, cb) {
-                cb(null, __dirname + "/public/")
-            },
-            filename: function (req, file, cb) {
-                var [loggedin, user] = isLoggedIn(req.cookies.session);
-                if (loggedin) {
-                    var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
-                    var fileArt = file.originalname.split(".");
-                    fileArt = fileArt[fileArt.length - 1];
-                    var fileCustomName = createFileName() + "." + fileArt;
-                    var path = __dirname + "/public/" + fileCustomName;
-                    if (req.body.fileName != undefined && req.body.fileName != "") {
-                        var newFile = { "name": req.body.fileName, "path": "/" + fileCustomName, "owner": user };
-                    } else {
-                        var newFile = { "name": file.originalname, "path": path, "owner": user };
-                    }
-                    usersJson[Object.keys(usersJson).length] = newFile;
-                    fs.writeFileSync(__dirname + "/userDB/" + user + ".json", Buffer.from(JSON.stringify(usersJson), "utf8"));
-                    return cb(null, fileCustomName);
-                } else {
-                    return cb(new Error('Not Logged in'));
-                }
-            }
-        })
-    };
+    const multerConfig = cloud.multer(multer);
     const upload = multer({ storage: multerConfig.storage })
 
+    app.get("/favicon.ico", (req, res) => {
+        res.sendFile(__dirname + "/static/pictures/schoki-logo.png");
+    });
+    app.get("/js/default.js", (req, res) => {
+        res.sendFile(__dirname + "/static/js/default.js");
+    });
+    app.get("/css/default.css", (req, res) => {
+        res.sendFile(__dirname + "/static/css/default.css");
+    });
+
     app.get('/', function (req, res) {
-        if (req.cookies.session == undefined) {
-            res.redirect("/login");
-        }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
-        if (loggedin) {
-            res.redirect("/home");
-        } else {
-            res.redirect("/login");
-        }
+        res.redirect("/login");
     })
 
     app.get('/login', function (req, res) {
         if (req.cookies.session == undefined) {
             res.sendFile(__dirname + "/static/login.html");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         if (loggedin) {
             res.redirect("/home");
         } else {
@@ -96,7 +52,7 @@ module.exports = function (app) {
                 }
             }
             if (loggedin) {
-                res.cookie('session', createSession(user), {});
+                res.cookie('session', cloud.createSession(user), {});
                 setTimeout(() => { }, 100);
                 res.redirect("/home");
             } else {
@@ -110,7 +66,7 @@ module.exports = function (app) {
         if (req.cookies.session == undefined) {
             res.redirect("/login");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         if (loggedin) {
             res.sendFile(__dirname + "/static/home.html");
         } else {
@@ -122,7 +78,7 @@ module.exports = function (app) {
         if (req.cookies.session == undefined) {
             res.redirect("/login");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         if (loggedin) {
             if (fs.existsSync(__dirname + "/userDB/" + user + ".json")) {
                 var usersDb = fs.readFileSync(__dirname + "/userDB/" + user + ".json") + ";" + user;
@@ -139,21 +95,11 @@ module.exports = function (app) {
         }
     })
 
-    app.get("/favicon.ico", (req, res) => {
-        res.sendFile(__dirname + "/static/pictures/schoki-logo.png");
-    });
-    app.get("/js/default.js", (req, res) => {
-        res.sendFile(__dirname + "/static/js/default.js");
-    });
-    app.get("/css/default.css", (req, res) => {
-        res.sendFile(__dirname + "/static/css/default.css");
-    });
-
     app.post('/download', upload.single("fileupload"), function (req, res) {
         if (req.cookies.session == undefined) {
             res.redirect("/login");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         if (loggedin) {
             res.redirect("/home");
         } else {
@@ -166,13 +112,13 @@ module.exports = function (app) {
             res.redirect("/login");
 
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         if (loggedin) {
-            console.log(__dirname + "/public/" + req.params.file);
             if (!fs.existsSync(__dirname + "/public/" + req.params.file)) {
+                log.log(user + " WANTED: " + req.params.file, 5);
                 res.sendFile(__dirname + "/public/didnotexistdefaultfile45437589043234778.txt");
             } else {
-                log.log("ASKING FILE: " + req.params.file, 5);
+                log.printlog(user + " DOWNLOADED: " + req.params.file, 3);
                 res.sendFile(__dirname + "/public/" + req.params.file);
             }
         } else {
@@ -180,11 +126,12 @@ module.exports = function (app) {
         }
     })
 
+    // TODO: Make this stuff here work
     app.put('/download/:file', function (req, res) {
         if (req.cookies.session == undefined) {
             res.redirect("/login");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         var owns = false;
         var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
         for (var i = 0; i < Object.keys(usersJson).length; i++) {
@@ -195,7 +142,7 @@ module.exports = function (app) {
         }
         if (loggedin) {
             if (!fs.existsSync(__dirname + "/public/" + req.params.file)) {
-                delFile("/" + req.params.file, user);
+                cloud.delFile("/" + req.params.file, user);
                 log.log(user + " deleted nonexistent /public/" + req.params.file, 1);
                 res.redirect("/home");
             } else {
@@ -214,7 +161,7 @@ module.exports = function (app) {
         if (req.cookies.session == undefined) {
             res.redirect("/login");
         }
-        var [loggedin, user] = isLoggedIn(req.cookies.session);
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
         var owns = false;
         var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
         for (var i = 0; i < Object.keys(usersJson).length; i++) {
@@ -225,7 +172,7 @@ module.exports = function (app) {
         }
         if (loggedin) {
             if (!fs.existsSync(__dirname + "/public/" + req.params.file)) {
-                delFile("/" + req.params.file, user);
+                cloud.delFile("/" + req.params.file, user);
                 log.log(user + " deleted nonexistent /public/" + req.params.file, 1);
                 res.redirect("/home");
             } else {
@@ -234,7 +181,7 @@ module.exports = function (app) {
                         if (err) throw err;
                     });
                 }
-                delFile("/" + req.params.file, user);
+                cloud.delFile("/" + req.params.file, user);
                 log.log(user + " deleted /public/" + req.params.file, 3);
                 res.redirect("/home");
             }
@@ -242,69 +189,5 @@ module.exports = function (app) {
             res.redirect("/login");
         }
     })
-
-    //TODO: Create put request for uploading files => Cloud finished xD
-    //      every file has property "owner" for deletion you have to be owner
-
-    /* MISSELANIOUS */
-    function createSession(user) {
-        var exists = true;
-        var num;
-        while (exists) {
-            num = Math.floor(Math.random() * 1E18);
-            for (var i = 0; i < sessions.length; i++) {
-                if (num == sessions[i].num) {
-                    exists = true;
-                }
-            }
-            sessions.push({ num, user });
-            break;
-        }
-        return num;
-    }
-
-    function isLoggedIn(session) {
-        for (var i = 0; i < sessions.length; i++) {
-            if (session == sessions[i].num) {
-                user = sessions[i].user;
-                return [true, sessions[i].user];
-            }
-        }
-        return [false, ""];
-    }
-
-    /* STORAGE */
-    function delFile(file, user) {
-        var usersJson = JSON.parse(fs.readFileSync(__dirname + "/userDB/" + user + ".json", "utf8"));
-        console.log(usersJson);
-        for (const [key, value] of Object.entries(usersJson)) {
-            if (value.path == file) {
-                console.log("PPPPP " + usersJson[key]);
-                delete usersJson[key];
-                console.log(usersJson);
-            }
-        }
-        fs.writeFileSync(__dirname + "/userDB/" + user + ".json", Buffer.from(JSON.stringify(usersJson), "utf8"));
-    }
-    function createFileName() {
-        var exists = true;
-        var num;
-        var dir = fs.readdirSync(__dirname + "/public");
-        var allexisting = [];
-        for (var i = 0; i < dir.length; i++) {
-            var name = dir[i].split(".");
-            name = name[0];
-            allexisting.push(name);
-        }
-        while (exists) {
-            num = Math.floor(Math.random() * 1000000000000000000);
-            for (var i = 0; i < allexisting.length; i++) {
-                if (num == allexisting[i].num) {
-                    exists = true;
-                } else { exists = false }
-            }
-        }
-        return num;
-    }
     return module;
 }
